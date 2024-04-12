620de4b3dc1a297d63f47491811898f9a6e342c1
Laibah-Shahid <laibahshahid18@gmail.com>
2024-04-12 23:30:05 +0500

    key updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
-import { fetchCars } from "@utils";
-import { HomeProps } from "@types";
-import { fuels, yearsOfProduction } from "@constants";
+"use client"
+
+import { fetchCars } from "@/utils";
+import { HomeProps } from "@/types";
+import { fuels, yearsOfProduction } from "@/constants";
 import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@/components";
 
 export default async function Home({ searchParams }: HomeProps) {
